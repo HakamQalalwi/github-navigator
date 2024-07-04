3bac6b612a0bb99028984259d0f681ac78da24a2
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-07-05 02:47:20 +0300

    Activate Search Method

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,36 +15,52 @@ const HomePage = () => {
 
   const [sortType, setSortType] = useState("forks");
 
-  const getUserProfileAndRepos = useCallback(async () => {
-    setLoading(true);
-    try {
-      const userRes = await fetch("https://api.github.com/users/HakamQalalwi");
-      const userProfile = await userRes.json();
-      setUserProfile(userProfile);
+  const getUserProfileAndRepos = useCallback(
+    async (username = "hakamqalalwi") => {
+      setLoading(true);
+      try {
+        const userRes = await fetch(`https://api.github.com/users/${username}`);
+        const userProfile = await userRes.json();
+        setUserProfile(userProfile);
 
-      const repoRes = await fetch(userProfile.repos_url);
-      const repos = await repoRes.json();
-      console.log("User Profile: ", userProfile);
-      console.log("Repos: ", repos);
-      setRepos(repos);
-    } catch (error) {
-      toast.error(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+        const repoRes = await fetch(userProfile.repos_url);
+        const repos = await repoRes.json();
+        setRepos(repos);
+        return { userProfile, repos };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     getUserProfileAndRepos();
   }, [getUserProfileAndRepos]);
 
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+  };
+
   return (
     <div className="m-4">
-      <Search />
+      <Search onSearch={onSearch} />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
-        {repos.length > 0 && !loading && <Repos repos={repos} />}
+        {!loading && <Repos repos={repos} />}
         {loading && <Spinner />}
         {/* <Spinner /> */}
       </div>
