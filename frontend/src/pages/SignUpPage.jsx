c9e9edb1123cfc7235669d53896806283f911de2
Hakam Qalalwi <73319756+HakamQalalwi@users.noreply.github.com>
2024-07-04 21:11:04 +0300

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const SignUpPage = () => {
 	return (
 		<div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0'>
-			<div className='w-full bg-glass rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0'>
+			<div className='w-full bg-glass rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 bg-glass'>
 				<div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
 					<h1 className='text-xl font-bold md:text-2xl text-center'>Create Account</h1>
 					<button
