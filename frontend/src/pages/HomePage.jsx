8fa33552c1f7083f18fcece103f0d29767bc7d5c
Hakam Qalalwi <73319756+HakamQalalwi@users.noreply.github.com>
2024-07-04 21:29:52 +0300

    Design Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,16 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+	return (
+		<div className='m-4'>
+			<Search />
+			<SortRepos />
+			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+				<ProfileInfo />
+				<Repos />
+				<Spinner />
+			</div>
+		</div>
+	);
 };
+
 
 export default HomePage;
