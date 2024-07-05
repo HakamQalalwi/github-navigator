899ebb7a351b2aa277a9f2ddc5b93f0ece620b52
Hakam Qalalwi <hakamqalalwi@outlook.com>
2024-07-05 03:36:28 +0300

    Manage Repos with methods most recent, stars, and forks

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -26,6 +26,7 @@ const HomePage = () => {
         const repoRes = await fetch(userProfile.repos_url);
         const repos = await repoRes.json();
         setRepos(repos);
+        console.log(userProfile);
         return { userProfile, repos };
       } catch (error) {
         toast.error(error.message);
@@ -54,10 +55,22 @@ const HomePage = () => {
     setLoading(false);
   };
 
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count);
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
+
   return (
     <div className="m-4">
       <Search onSearch={onSearch} />
-      <SortRepos />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
         {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
         {!loading && <Repos repos={repos} />}
